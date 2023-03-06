990942c45667d1c8ab1394621efe1387c2368c37
himanipgs <himani.chauhan@potenzaglobalsolutions.com>
2023-03-06 14:58:10 +0530

    commit in new branch

--- test/Controllers/HomeController.cs
+++ test/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace test.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
